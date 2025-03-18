4b202d6e65e97fa945fa44e47bd86e330d9ffaaf
Mojgan Madadikhaljan <70570300+mojganmadadi@users.noreply.github.com>
2025-03-18 22:17:23 +0100

    Update dataloader.py

--- src/dataloader.py
+++ src/dataloader.py
@@ -17,7 +17,7 @@ class RegionizedDataset(Dataset):
         self.data_all = pd.read_csv(csv_file).dropna()
         self.data_all["region_index"] = self.data_all["region_index"].astype(int)
         self.which_class_gv = 10 if which_class=="forest" else 80 if which_class=="water" else 40 if which_class=="crop" else 30 if which_class=="grass" else None
-        self.data_all = self.data_all[self.data_all[f"%_{self.which_class_gv}"]>10]
+        # self.data_all = self.data_all[self.data_all[f"%_{self.which_class_gv}"]>10] # This is to have images with enough amount of interesting classes inside
 
         self.transforms = transforms.Compose([transforms.CenterCrop(256)])
         self.data = self.data_all.reset_index(drop=True)[:]
@@ -48,49 +48,6 @@ class RegionizedDataset(Dataset):
             'image':  self.transforms(torch.as_tensor(img.copy())).float().contiguous(),
             'mask':  self.transforms(torch.as_tensor(mask.copy())).long().contiguous(),
             'region_index': torch.as_tensor(region_index).long().contiguous(),
-            # 'weight': torch.as_tensor(weight).float().contiguous(),
-        }
-
-
-
-class RegionizedDataset_test(Dataset):
-    def __init__(self, csv_file, transform, images_dir: str, mask_dir: str , train_or_test="train"):
-        self.data_all = pd.read_csv(csv_file).dropna()
-        self.data_all["region_index"] = self.data_all["region_index"].astype(int)
-
-
-
-        self.transforms = transforms.Compose([transforms.CenterCrop(256)])
-        self.data = self.data_all.reset_index(drop=True)[:]
-            
-
-        self.images_dir = Path(images_dir)
-        self.mask_dir = Path(mask_dir)
-        self.ids = self.data["point_index"]
-
-
-
-    def __len__(self):
-        return len(self.data)
-
-
-    def __getitem__(self, ids):
-
-        name = str(self.data.point_index[ids])
-        region_index = self.data.region_index[ids]
-        # weight = self.data.weight[ids]
-        mask_file = list(self.mask_dir.glob(name + '.*'))
-        img_file = list(self.images_dir.glob(name + '.*'))
-        assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
-        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
-        mask = load_test_image(mask_file[0], mask=True)
-        img = load_test_image(img_file[0], mask=False)
-
-        return {
-            'image':  self.transforms(torch.as_tensor(img.copy())).float().contiguous(),
-            'mask':  self.transforms(torch.as_tensor(mask.copy())).long().contiguous(),
-            'region_index': torch.as_tensor(region_index).long().contiguous(),
-            'image_id': torch.as_tensor(self.data.point_index[ids]).long().contiguous(),
             # 'weight': torch.as_tensor(weight).float().contiguous(),
         }
 
